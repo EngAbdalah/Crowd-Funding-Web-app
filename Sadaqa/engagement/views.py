a9fa39b7b521aefa57dc99a50c74208c4198c45a
anwer <theonemyth99@gmail.com>
2025-05-30 08:37:18 +0300

    serializers done

--- Sadaqa/engagement/views.py
+++ Sadaqa/engagement/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import Comment, Reply, Rate
+from .serializers import CommentSerializer, ReplySerializer, RateSerializer
 # Create your views here.
