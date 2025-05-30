9e3d6102f925a96dc6cb4254710d21760c207ea1
anwer <theonemyth99@gmail.com>
2025-05-30 08:18:38 +0300

    comment model created

--- Sadaqa/engagement/models.py
+++ Sadaqa/engagement/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from users.models import CustomUser
+from projects.models import Project
 # Create your models here.
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.project.title}"
