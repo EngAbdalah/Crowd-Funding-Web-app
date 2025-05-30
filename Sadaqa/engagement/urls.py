90a70ee4008561f9351d99574e4468f22aad2986
anwer <theonemyth99@gmail.com>
2025-05-30 11:01:18 +0300

    views updated 2

--- Sadaqa/engagement/urls.py
+++ Sadaqa/engagement/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('replies/<int:pk>/', reply_detail),
     path('rates/', rate_list),
     path('rates/<int:pk>/', rate_detail),
-    path('projects/<int:project_id>/comments/create/', create_comment, name='create-comment'),
-    path('comments/<int:comment_id>/replies/create/', create_reply, name='create-reply'),
 ]
