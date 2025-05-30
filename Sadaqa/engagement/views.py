5980fc8634445445d3add55baf01379d5f605182
anwer <theonemyth99@gmail.com>
2025-05-30 08:38:38 +0300

    views starts, comment list view done

--- Sadaqa/engagement/views.py
+++ Sadaqa/engagement/views.py
@@ -4,3 +4,18 @@ from rest_framework import status, permissions
 from .models import Comment, Reply, Rate
 from .serializers import CommentSerializer, ReplySerializer, RateSerializer
 # Create your views here.
+@api_view(['GET', 'POST'])
+@permission_classes([permissions.IsAuthenticatedOrReadOnly])
+def comment_list(request):
+    if request.method == 'GET':
+        project_id = request.query_params.get('project_id')
+        comments = Comment.objects.filter(project_id=project_id) if project_id else Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
