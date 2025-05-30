04a395d16010552d47d673b00ad7d4acddbb6112
anwer <theonemyth99@gmail.com>
2025-05-30 08:19:29 +0300

    reply model created

--- Sadaqa/engagement/models.py
+++ Sadaqa/engagement/models.py
@@ -11,3 +11,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.user.username} on {self.project.title}"
+
+class Reply(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Reply by {self.user.username} to {self.comment}"
